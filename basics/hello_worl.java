fd57ec7c9e66ddcbf2b3d7c89c9fd6427b22975b
Francisco Eduardo Balart Sanchez <balart40@hotmail.com>
2016-09-19 11:23:16 -0500

    Create hello_worl.java

---
+++ basics/hello_worl.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World take me to your leader, you base belong to us");
+	}
+
+}
